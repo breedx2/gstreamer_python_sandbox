b9c5894c50d68825c0fa1f5678f2661150c75e28
Jason Plumb <jason@noisybox.net>
2012-11-06 23:08:39 -0800

    Decrease cognitive load by separating display/gui crap from actual gstreamer crap.  Yeah.

--- hello_gstreamer.py
+++ hello_gstreamer.py
@@ -9,8 +9,49 @@ import gst
 default_file = '/home/jason/Oregon_Painting_Society_2011-04-08.avi'
 
 class CustomPlayer(object):
-	def __init__(self):
-		pass
+	def __init__(self, message_callback, sync_message_callback):
+		self.player = gst.element_factory_make("playbin2", "player")
+		bus = self.player.get_bus()
+		bus.add_signal_watch()
+		bus.enable_sync_message_emission()
+		bus.connect("message", self._on_message)
+		bus.connect("sync-message::element", self._on_sync_message)
+
+		bin = gst.Bin("my-bin")
+		timeoverlay = gst.element_factory_make("timeoverlay")
+		bin.add(timeoverlay)
+		pad = timeoverlay.get_pad("video_sink")
+		ghostpad = gst.GhostPad("sink", pad)
+		bin.add_pad(ghostpad)
+		autovideosink = gst.element_factory_make("autovideosink")
+		bin.add(autovideosink)
+
+		gst.element_link_many(timeoverlay, autovideosink)
+		self.player.set_property("video-sink", bin)
+
+		self.message_callback = message_callback
+		self.sync_message_callback = sync_message_callback
+
+	def start(self, filepath):
+		self.player.set_property("uri", "file://" + filepath)
+		self.player.set_state(gst.STATE_PLAYING)
+
+	def stop(self):
+		self.player.set_state(gst.STATE_NULL)
+
+	def _on_message(self, bus, message):
+		print "DEBUG on_message: %s" %(message.type)
+		if message.type in [gst.MESSAGE_EOS, gst.MESSAGE_ERROR]:
+			self.stop()
+		if message.type == gst.MESSAGE_ERROR:
+			err, debug = message.parse_error()
+			print "Error: %s" % err, debug
+		self.message_callback(bus, message)
+
+	def _on_sync_message(self, bus, message):
+		if message.structure is None:
+			return
+		self.sync_message_callback(bus, message)
 
 class GtkMain:
 	
@@ -32,52 +73,25 @@ class GtkMain:
 		self.movie_window = gtk.DrawingArea()
 		vbox.add(self.movie_window)
 		window.show_all()
+
+		self.player = CustomPlayer(self.on_message, self.on_sync_message)
 		
-		self.player = gst.element_factory_make("playbin2", "player")
-		bus = self.player.get_bus()
-		bus.add_signal_watch()
-		bus.enable_sync_message_emission()
-		bus.connect("message", self.on_message)
-		bus.connect("sync-message::element", self.on_sync_message)
-
-		bin = gst.Bin("my-bin")
-		timeoverlay = gst.element_factory_make("timeoverlay")
-		bin.add(timeoverlay)
-		pad = timeoverlay.get_pad("video_sink")
-		ghostpad = gst.GhostPad("sink", pad)
-		bin.add_pad(ghostpad)
-		autovideosink = gst.element_factory_make("autovideosink")
-		bin.add(autovideosink)
-
-		gst.element_link_many(timeoverlay, autovideosink)
-		self.player.set_property("video-sink", bin)
 		
 	def start_stop(self, w):
 		if self.button.get_label() == "Start":
 			filepath = self.entry.get_text()
 			if os.path.isfile(filepath):
 				self.button.set_label("Stop")
-				self.player.set_property("uri", "file://" + filepath)
-				self.player.set_state(gst.STATE_PLAYING)
+				self.player.start(filepath)
 		else:
-			self.player.set_state(gst.STATE_NULL)
+			self.player.stop()
 			self.button.set_label("Start")
 						
 	def on_message(self, bus, message):
-		t = message.type
-		print "DEBUG on_message: %s" %(message.type)
-		if t == gst.MESSAGE_EOS:
-			self.player.set_state(gst.STATE_NULL)
-			self.button.set_label("Start")
-		elif t == gst.MESSAGE_ERROR:
-			self.player.set_state(gst.STATE_NULL)
-			err, debug = message.parse_error()
-			print "Error: %s" % err, debug
+		if message.type in [gst.MESSAGE_EOS, gst.MESSAGE_ERROR]:
 			self.button.set_label("Start")
 	
 	def on_sync_message(self, bus, message):
-		if message.structure is None:
-			return
 		message_name = message.structure.get_name()
 		if message_name == "prepare-xwindow-id":
 			imagesink = message.src
